ac17bc301a5b0c7f84c32d1f68374fec97b87a39
Junya Kono <junya03dance@gmail.com>
2023-10-18 00:23:53 +0900

    fix: prettier setting
    close #1

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import { Hono } from "hono";
-import { bearerAuth } from "hono/bearer-auth";
-import { OpenAI } from "./openai";
+import {Hono} from 'hono';
+import {bearerAuth} from 'hono/bearer-auth';
+import {OpenAI} from './openai';
 
 const app = new Hono();
 const ai = new OpenAI();
 
 // health check
-app.get("/health", (c) => c.text("OK"));
+app.get('/health', c => c.text('OK'));
 
-app.post("/ai/*", async (c, next) => {
+app.post('/ai/*', async (c, next) => {
   // TODO: want to add a type to Hono's c.env.
-  const bearer = bearerAuth({ token: c.env!["TOKEN"] as string });
+  const bearer = bearerAuth({token: c.env!['TOKEN'] as string});
   return bearer(c, next);
 });
-app.post("/ai/query", async (c) => {
+app.post('/ai/query', async c => {
   // TODO: want to add a type to Hono's c.env.
-  const apiKey = c.env!["OPENAI_API_KEY"] as string;
+  const apiKey = c.env!['OPENAI_API_KEY'] as string;
   const body = await c.req.text();
   const message = await ai.createCompletion(apiKey, body);
 
-  return c.json({ message: message });
+  return c.json({message: message});
 });
 
 export default app;
